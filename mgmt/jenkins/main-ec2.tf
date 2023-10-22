c2d3aff52e9ff5016bb95fc46e8b7ac035836fb3
MostafaHassan724 <mostafahassan725@gmail.com>
2023-10-22 03:50:24 +0300

    slight modification

--- mgmt/jenkins/main-ec2.tf
+++ mgmt/jenkins/main-ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "this_jenkins" {
   instance_type          = var.instance_type
   availability_zone = var.az
   subnet_id = var.subnet_id
-  vpc_security_group_ids = [ aws_security_group.myjenkins_sg.id ]
+  vpc_security_group_ids = [ aws_security_group.jenkins_sg.id ]
   key_name        = var.key_name
 
   user_data = file("install-jenkins.sh")
